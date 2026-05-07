3d3dff70128eb5217007c2074e8d2edc6059cf8b
miguelmartinezcano <miguel.martinezcano3@gmail.com>
2026-05-07 15:27:27 -0400

    initial todo store

---
+++ src/app/model/mock-data.ts
@@ -0,0 +1,29 @@
+import { Todo } from "./todo.model";
+
+export const TODOS: Todo[] = [
+  {
+    id: 1,
+    title: "Learn Angular",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Learn TypeScript",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Build a Todo App with Angular and NgRx Signals",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Deploy the app to production",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "Celebrate!",
+    completed: false,
+  },
+];
