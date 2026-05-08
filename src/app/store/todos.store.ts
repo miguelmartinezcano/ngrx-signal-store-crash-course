e2554de9cca93944082e3a0204199c0642f13cdf
miguelmartinezcano <miguel.martinezcano3@gmail.com>
2026-05-08 15:54:30 -0400

    completed course

--- src/app/store/todos.store.ts
+++ src/app/store/todos.store.ts
@@ -1,11 +1,12 @@
 import { Todo } from "../model/todo.model";
-import { signalStore, withState, withMethods, patchState } from "@ngrx/signals";
+import { signalStore, withState, withMethods, patchState, withComputed } from "@ngrx/signals";
 import { TodosService } from "../services/todos.service";
 import { inject } from "@angular/core";
+import { computed } from "@angular/core";
 
-export type TodosFilter = 'all' | 'active' | 'completed';
+export type TodosFilter = 'all' | 'pending' | 'completed';
 
-type TodosState = {
+export type TodosState = {
     todos: Todo[];
     loading: boolean;
     filter: TodosFilter;
@@ -20,13 +21,48 @@ const initialState: TodosState = {
 export const TodosStore = signalStore(
     {providedIn: 'root'},
     withState(initialState),
+    withComputed((state) => ({
+        filteredTodo: computed(() => {
+            const todos = state.todos();
+            switch (state.filter()) {
+                case 'all':
+                    return todos;
+                case 'pending':
+                    return todos.filter(todo => !todo.completed);
+                case 'completed':
+                    return todos.filter(todo => todo.completed);
+            }
+        })
+    })),
     withMethods(
         (store, todosService = inject(TodosService)) => ({
           async loadAll() {
             patchState(store, { loading: true });
             const todos = await todosService.getTodos();
             patchState(store, { todos, loading: false });
-          }  
+          },  
+          async addTodo(title: string) {
+            const todo = await todosService.addTodo({ title, completed: false });
+            patchState(store, (state) => ({
+              todos: [...state.todos, todo]
+            }));
+          },
+          async deleteTodo(id: number) {
+            await todosService.deleteTodo(id);
+            patchState(store, (state) => ({
+              todos: state.todos.filter(todo => todo.id !== id)
+            }));
+          },
+          async updateTodo(id: number, completed: boolean) {
+            await todosService.updateTodo(id, completed);
+            patchState(store, (state) => ({
+              todos: state.todos.map(todo => 
+                todo.id === id ? { ...todo, completed } : todo)
+            }));
+          },
+          updateFilter(filter: TodosFilter) {
+            patchState(store, { filter });
+          }
         })
     )
 )
--- src/app/todos-list/todos-list.ts
+++ src/app/todos-list/todos-list.ts
@@ -1,20 +1,50 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, viewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSuffix } from '@angular/material/form-field';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
 import { TodosStore } from '../store/todos.store';
 import { MatSelectionList } from '@angular/material/list';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatListOption } from '@angular/material/list';
+import { MatButtonToggleGroup } from '@angular/material/button-toggle';
+import { TodosFilter } from '../store/todos.store';
 
 @Component({
   selector: 'todos-list',
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatSuffix, MatButtonToggleModule, MatSelectionList, MatCheckboxModule, MatListOption],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatSuffix, MatButtonToggleModule, MatSelectionList, MatCheckboxModule, MatListOption, MatButtonToggleGroup],
   templateUrl: './todos-list.html',
   styleUrl: './todos-list.scss',
 })
 export class TodosList {
   store = inject(TodosStore);
+
+  filter = viewChild.required(MatButtonToggleGroup);
+
+  constructor() {
+    effect(() => {
+      const filter = this.filter();
+      filter.value = this.store.filter();
+    });
+  }
+
+  async onAddTodo(title: string) {
+    await this.store.addTodo(title);
+  }
+
+  async onDeleteTodo(id: number, event: MouseEvent) {
+    event.stopPropagation();
+    await this.store.deleteTodo(id);
+  }
+
+  async onTodoToggled(id: number, completed: boolean) {
+    await this.store.updateTodo(id, completed);
+  }
+
+  onFilterTodos(event: MatButtonToggleChange) {
+    const filter = event.value as TodosFilter;
+    this.store.updateFilter(filter);
+  }
 }
